553910b95f2e8e32e76c1408b7c8e4747d3976a9
Your Name <youremail@example.com>
2025-07-27 15:24:27 +0530

    modified

--- server.js
+++ server.js
@@ -6,7 +6,12 @@ import { connectDB } from './config/db.js';
 
 dotenv.config();
 const app = express()
-app.use(cors());
+app.use(cors({
+  origin: 'https://frontend-inventory-eta.vercel.app', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
+
 
 const PORT = process.env.PORT || 4000
 
